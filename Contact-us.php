936504980a8590c53954e7cbdd724cd98989ef20
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-12-28 21:26:04 +0430

    new files added

--- Contact-us.php
+++ Contact-us.php
@@ -20,6 +20,7 @@ include_once('./_Partial Components/Header.php');
                                     echo "<div class='alert alert-success' role='alert' style = 'font-size: 16px;'> $msg </div>";
                                 }
                             ?>  
+                            
                     </div>
                     <div class="form-group">
                         <label for="ful-name"> Full Name </label>
