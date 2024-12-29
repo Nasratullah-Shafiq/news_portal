dfad0ab1e9d7ec2acaa89980d98cfa63bce1a60d
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-12-29 15:13:02 +0430

    new files added

--- Administrator/_Partial Components/Footer.php
+++ Administrator/_Partial Components/Footer.php
@@ -20,6 +20,7 @@
         <script src="./js/AjaxSearch.js"></script>
         
     <footer class ="footer">
+        
         <div id="container-fluid">
             Copyright &copy; by <a href="http://www.facebook.com/Nasratullah Shafiq"> Nasratullah Shafiq </a>. All Right Reserved from 2011 - <?php echo date('Y');?>.
         </div>            
