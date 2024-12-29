e316dcd35457e157468d9d30d6f0a0e5cfc04236
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-12-29 14:34:37 +0430

    new files added

--- Contact-us.php
+++ Contact-us.php
@@ -13,6 +13,7 @@ include_once('./_Partial Components/Header.php');
 				<form action="" method = "POST">
                     <div class="form-group">
                             <?php 
+                            
                                 if (isset($error)) {
                                     echo "<div class='alert alert-danger' role='alert' style = 'font-size: 16px;'> $error </div>";
                                 }
@@ -20,7 +21,7 @@ include_once('./_Partial Components/Header.php');
                                     echo "<div class='alert alert-success' role='alert' style = 'font-size: 16px;'> $msg </div>";
                                 }
                             ?>  
-                            
+
                     </div>
                     <div class="form-group">
                         <label for="ful-name"> Full Name </label>
