e6c9aa85c8e3743869316444912de53a7c181664
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-01-10 00:08:50 +0430

    new changes

--- Administrator/Ajax Search/editCategory.php
+++ Administrator/Ajax Search/editCategory.php
@@ -5,7 +5,6 @@
 
 	if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		 
-		
 		$Category_ID = $_POST['Category_ID'];
 		$Category = $_POST['Category'];
 		$updtCategory = $usr->updateCategory($Category_ID, $Category);
