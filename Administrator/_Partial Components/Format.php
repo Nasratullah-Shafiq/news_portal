e4dedf21babccc0ffc009d8be72c2180ee47d4a2
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-01-11 08:13:43 +0430

    new changes

--- Administrator/_Partial Components/Format.php
+++ Administrator/_Partial Components/Format.php
@@ -5,7 +5,7 @@
 class Format{
  public function formatDate($date){
   return date('F j, Y, g:i a', strtotime($date));
- }
+ } 
 
  public function textShorten($text, $limit = 400){
   $text = $text. " ";
