a9a5fb611a2f898cd0de98414c789425ee7db9d6
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-01-11 08:13:18 +0430

    new changes

--- Administrator/_Partial Components/Footer.php
+++ Administrator/_Partial Components/Footer.php
@@ -18,7 +18,7 @@
         <script src="./js/transition.js"></script>
         <script src = "./js/AdminOnlineQuiz.js"></script>
         <script src="./js/AjaxSearch.js"></script>
-        
+         
     <footer class ="footer">
         <div id="container-fluid">
             Copyright &copy; by <a href="http://www.facebook.com/Nasratullah Shafiq"> Nasratullah Shafiq </a>. All Right Reserved from 2011 - <?php echo date('Y');?>.
